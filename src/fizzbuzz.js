1fd8deab6e1d6160edbdfa8274d26da481db508b
Aura Salazar <aurasader23@gmail.com>
2026-05-08 17:06:27 -0600

    Se agrego el código de aura

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,7 +1,19 @@
 export function fizzbuzz(){
   //tu código va aqui
- 
-  
+
+ let counter = 1;
+ while (counter <= 100) {
+  if (counter % 3 === 0 && counter % 5 === 0) {
+  console.log("FizzBuzz");
+  } else if (counter % 3 === 0) {
+    console.log("Fizz");
+  } else if (counter % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(counter);
+  }
+  counter = counter + 1;
+}
 }
 
 fizzbuzz();
